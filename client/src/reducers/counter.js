b045761144914af4126c18f3065f7a38a42bddcb
Vedant Panchal <vedantpanchal12345@gmail.com>
2023-07-14 13:20:55 +0530

    userDetails reducer TODO

--- client/src/reducers/counter.js
+++ client/src/reducers/counter.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const counterSlice = createSlice({
-  name: "counter",
+  name: "counter1",
   initialState: 0,
   reducers: {
     increment: (state) => state + 1,
@@ -11,3 +11,7 @@ const counterSlice = createSlice({
 
 export const { increment, decrement } = counterSlice.actions;
 export const counterReducer = counterSlice.reducer;
+
+// const [test, settest] = useState(0);
+
+// settest(1);
--- client/src/store.js
+++ client/src/store.js
@@ -5,6 +5,7 @@ import { counterReducer } from "./reducers/counter";
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    // userDetails: userDetailsReducer, //TODO
     // Add other slice reducers here
   },
 });
