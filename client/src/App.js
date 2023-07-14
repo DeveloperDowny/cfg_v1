c8e4c7efcf60c2b2e603e5cb0834830c69aec05d
Vedant Panchal <vedantpanchal12345@gmail.com>
2023-07-14 13:06:39 +0530

    redux added

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 import Home from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TestComponent from "./components/test";
+import Test2 from "./components/Test2";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/test2" element={<Test2 />} />
         <Route path="/test" element={<TestComponent />} />
       </Routes>
     </BrowserRouter>
---
+++ client/src/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { counterReducer } from "./reducers/counter";
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    // Add other slice reducers here
+  },
+});
+
+export default store;
+
+// Create a hook for useDispatch
+export const useAppDispatch = () => useDispatch();
